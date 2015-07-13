b52cc5652ad80609ac4e78a89328b81e17675840
Epicurean306 <benjaminforbesgriffith@gmail.com>
2015-07-13 12:28:56 -0400

    continuing work on some profile-form functionality for each user based on codepen tutorial

--- app/scripts/controllers/ProfileController.js
+++ app/scripts/controllers/ProfileController.js
@@ -1,18 +1,28 @@
 (function() {
   'use strict';
-  angular.module('form-demo', [])
+  angular.module('profileForm', [])
     .controller('MainController', function() {
       alert('it lives!');
 
-      this.submission = { };
+      this.submission = {};
 
-            this.saveSubmission = function(submission){
-              this.log(submission);
-            }
+      /**
+       * @param {FormController} $form submitted
+       */
+      this.saveSubmission = function($form) {
+        if (!$form.$dirty || !$form.$valid) return;
+        // Do nothing is nothing is submitted or if invalid
 
-            this.log = function(){
-              console.log(arguments);
-              this.logged = JSON.stringify(arguments, null, 2);
+        this.submission.created_on = new Date;
+
+        this.log(this.submission);
+
+        this.submission = {}; // Reset the form...
+      }
+
+      this.log = function() {
+        console.log(arguments);
+        this.logged = JSON.stringify(arguments, null, 2);
       };
     })
 })();
