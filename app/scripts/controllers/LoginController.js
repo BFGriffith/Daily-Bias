c374dc333fd93cf2df4a40e3eb6099fc207f83d4
Epicurean306 <benjaminforbesgriffith@gmail.com>
2015-07-13 00:06:25 -0400

    working on trying to figure out some of the JavaScript for Google OAuth login with Firebase

---
+++ app/scripts/controllers/LoginController.js
@@ -0,0 +1,30 @@
+(function() {
+  'use strict';
+  var ref = new Firebase("https://spin-zone.firebaseio.com");
+ref.authWithOAuthRedirect("google", function(error) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    // We'll never get here, as the page will redirect on success.
+  }
+});
+/**TODO: create a callback which logs the current auth state
+    * @param ?
+    * @return ?
+    */
+function authDataCallback(authData) {
+  if (authData) {
+    console.log("User " + authData.uid + " is logged in with " + authData.provider);
+  } else {
+    console.log("User is logged out");
+  }
+}
+
+/**TODO: register the callback to be fired every time auth state changes
+    * @param ?
+    * @return ?
+    */
+var ref = new Firebase("https://spin-zone.firebaseio.com");
+ref.onAuth(authDataCallback);
+
+})();
