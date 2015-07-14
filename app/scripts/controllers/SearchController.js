ad79aed667c1613182c320c57f624ab2b33de90c
Epicurean306 <benjaminforbesgriffith@gmail.com>
2015-07-14 14:59:13 -0400

    trying a different tactic for GoogleNewsSearch results

--- app/scripts/controllers/SearchController.js
+++ app/scripts/controllers/SearchController.js
@@ -2,6 +2,50 @@
     'use strict';
     //angular.module('app')
 
+    google.load('search', '1');
+
+          var newsSearch;
+
+          function searchComplete() {
+
+            // Check that we got results
+            document.getElementById('content').innerHTML = '';
+            if (newsSearch.results && newsSearch.results.length > 0) {
+              for (var i = 0; i < newsSearch.results.length; i++) {
+
+                // Create HTML elements for search results
+                var p = document.createElement('p');
+                var a = document.createElement('a');
+                a.href="/news-search/v1/newsSearch.results[i].url;"
+                a.innerHTML = newsSearch.results[i].title;
+
+                // Append search results to the HTML nodes
+                p.appendChild(a);
+                document.body.appendChild(p);
+              }
+            }
+          }
+
+          function onLoad() {
+
+            // Create a News Search instance.
+            newsSearch = new google.search.NewsSearch();
+
+            // Set searchComplete as the callback function when a search is
+            // complete.  The newsSearch object will have results in it.
+            newsSearch.setSearchCompleteCallback(this, searchComplete, null);
+
+            // Specify search quer(ies)
+            newsSearch.execute('Barack Obama');
+
+            // Include the required Google branding
+            google.search.Search.getBranding('branding');
+          }
+
+          // Set a callback to call your code when the page loads
+          google.setOnLoadCallback(onLoad);
+
+/*
     angular.module('spin-zone'('SearchController'))
         .controller('SearchController', function($scope, $http) {
           var self = this;
@@ -12,7 +56,7 @@
               self.news = response.data;
             });
         });
-
+*/
 /*
   $( "#search" ).click(function() {
     alert( "click-handler called" );
