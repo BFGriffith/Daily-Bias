cb5c23437cfd062f8f792bc619bda20004b4c033
Epicurean306 <benjaminforbesgriffith@gmail.com>
2015-07-15 09:53:16 -0400

    repo local, remote, and firebase name changed... still working on search

--- app/scripts/controllers/SearchController.js
+++ app/scripts/controllers/SearchController.js
@@ -2,49 +2,22 @@
     'use strict';
     //angular.module('app')
 
-    google.load('search', '1');
+$(document).ready(function(){
+    $.getJSON("http://www.faroo.com/api?q=iphone&start=1&length=10&l=en&src=news&f=json",
+        function(data){
+          $.each(data.results, function(i,item){
+			jQuery('<div/>', {  text: item.title }).appendTo('#results');
+          });
+        });
+  });
 
-          var newsSearch;
+/*
+    .controller('SearchController', function($scope, $http) {
+    return $resource('https://ajax.googleapis.com/ajax/services/search/news' + '?MSNBC', {}, {
 
-          function searchComplete() {
-
-            // Check that we got results
-            document.getElementById('content').innerHTML = '';
-            if (newsSearch.results && newsSearch.results.length > 0) {
-              for (var i = 0; i < newsSearch.results.length; i++) {
-
-                // Create HTML elements for search results
-                var p = document.createElement('p');
-                var a = document.createElement('a');
-                a.href="/news-search/v1/newsSearch.results[i].url;"
-                a.innerHTML = newsSearch.results[i].title;
-
-                // Append search results to the HTML nodes
-                p.appendChild(a);
-                document.body.appendChild(p);
-              }
-            }
-          }
-
-          function onLoad() {
-
-            // Create a News Search instance.
-            newsSearch = new google.search.NewsSearch();
-
-            // Set searchComplete as the callback function when a search is
-            // complete.  The newsSearch object will have results in it.
-            newsSearch.setSearchCompleteCallback(this, searchComplete, null);
-
-            // Specify search quer(ies)
-            newsSearch.execute('Barack Obama');
-
-            // Include the required Google branding
-            google.search.Search.getBranding('branding');
-          }
-
-          // Set a callback to call your code when the page loads
-          google.setOnLoadCallback(onLoad);
-
+            });
+        })
+*/
 /*
     angular.module('spin-zone'('SearchController'))
         .controller('SearchController', function($scope, $http) {
