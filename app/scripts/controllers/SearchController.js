ae6b33bbe1bd8b96380bd09f594763586ed54b9f
Epicurean306 <benjaminforbesgriffith@gmail.com>
2015-07-17 17:56:18 -0400

    cleaned up old code, still not able to get json or jsonp with GoogleNewsSearch. Contemplating pivot to some other search API

--- app/scripts/controllers/SearchController.js
+++ app/scripts/controllers/SearchController.js
@@ -21,25 +21,33 @@ this.submitSearch = function(){
 
 
 /*
-  function loadApi() {
-    google.load("search", "1", {
-      "callback": pageLoaded
-    });
-  }
+.controller('SearchController', function($http) {
+    var self = this;
+    self.search = [];
+
+
+this.submitSearch = function(){
+    $http.jsonp('https://ajax.googleapis.com/ajax/services/search/news?v=1.0&q=barack%20obama&callback=JSON_CALLBACK')
+      .then(function(response) {
+        console.log(response);
+        self.search = response.data;
+      });
+    };
+  });
 */
 
 /*
   angular.module('daily-bias')
 
   .controller('SearchController', function submitSearch($http) {
-      $http.get('https://ajax.googleapis.com/ajax/services/search/news' + '?callback=JSON_CALLBACK', {
+      $http.get('https://ajax.googleapis.com/ajax/services/search/news?' + 'callback=JSON_CALLBACK', {
           params: {
             "version": "1.0",
             "language": "en"
           }
           .then
                 console.log();
-              });//END params
+              });
 
         };
       };
