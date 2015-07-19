95fe0e7e992a07870d5dc3b4df7fe06d5b11563d
Epicurean306 <benjaminforbesgriffith@gmail.com>
2015-07-19 14:37:11 -0400

    still cannot get vanillaJS GoogleNews code to work

--- app/scripts/controllers/SearchController.js
+++ app/scripts/controllers/SearchController.js
@@ -1,8 +1,58 @@
 (function() {
   'use strict';
 
-  
+  angular.module('daily-bias')
+  .controller('SearchController', function() {
 
+  this.submitSearch = function(){
+
+  google.load('search', '1');
+
+        var newsSearch;
+
+        function searchComplete() {
+
+          // Check that we got results
+          document.getElementById('content').innerHTML = '';
+          if (newsSearch.results && newsSearch.results.length > 0) {
+            for (var i = 0; i < newsSearch.results.length; i++) {
+
+              // Create HTML elements for search results
+              var p = document.createElement('p');
+              var a = document.createElement('a');
+              a.href="/news-search/v1/newsSearch.results[i].url;"
+              a.innerHTML = newsSearch.results[i].title;
+
+              // Append search results to the HTML nodes
+              p.appendChild(a);
+              document.body.appendChild(p);
+            }
+          }
+        }
+
+        function onLoad() {
+
+          // Create a News Search instance.
+          newsSearch = new google.search.NewsSearch();
+
+          // Set searchComplete as the callback function when a search is
+          // complete.  The newsSearch object will have results in it.
+          newsSearch.setSearchCompleteCallback(this, searchComplete, null);
+
+          // Specify search quer(ies)
+          newsSearch.execute('Barack Obama');
+
+          // Include the required Google branding
+          google.search.Search.getBranding('branding');
+        }
+
+        // Set a callback to call your code when the page loads
+        google.setOnLoadCallback(onLoad);
+
+  }
+});
+
+/*
   angular.module('daily-bias')
   .controller('SearchController', function($http) {
       var self = this;
@@ -17,7 +67,7 @@ this.submitSearch = function(){
         });
       };
     });
-
+*/
 
 /*
 .controller('SearchController', function($http) {
