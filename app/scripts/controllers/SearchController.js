61e20a49b5046b735f2e1c7c3a31a13651b028dc
Epicurean306 <benjaminforbesgriffith@gmail.com>
2015-07-20 15:16:35 -0400

    finally got search data in the DOM with FAROO api... will curl some files with that path and work $http.get with locally-cached path for now to keep the API-key secure until I can figure out live calls

--- app/scripts/controllers/SearchController.js
+++ app/scripts/controllers/SearchController.js
@@ -1,9 +1,49 @@
-/*
 (function() {
     'use strict';
-    //angular.module('daily-bias', ['ui.router']);
+    angular.module('daily-bias')
+
+    .controller('SearchController', function($http) {
+
+        this.searchResults = [];
+        var self = this;
+
+      /*
+        this.submitSearch = function() {
+            $(document).ready(function() {
+            */
 
 
+            $http.get("/api/left.json")
+              .then (function(response) {
+                // $.each(data.results, function(i, item) {
+                //   jQuery('<div/>', {
+                //     text: item.title
+                //   }).appendTo('#results');
+                // });
+                self.searchResults = response.data.results;
+                console.log(response);
+                console.log(self.searchResults);
+              });
+
+          });// END SearchController
+        })(); //END IIFE
+/*
+          function localJsonpCallback(json) {
+                  if (!json.Error) {
+                      $('#resultForm').submit();
+                  }
+                  else {
+                      $('#loading').hide();
+                      $('#userForm').show();
+                      alert(json.Message);
+                  }
+              }
+*/
+
+      //  }); //END SearchController function
+
+
+  /*
   angular.module('daily-bias')
   .controller('SearchController', function() {
 
@@ -73,38 +113,35 @@ this.submitSearch = function(){
     });
 */
 
-/*
-.controller('SearchController', function($http) {
-    var self = this;
-    self.search = [];
+  /*
+  .controller('SearchController', function($http) {
+      var self = this;
+      self.search = [];
 
 
-this.submitSearch = function(){
-    $http.jsonp('https://ajax.googleapis.com/ajax/services/search/news?v=1.0&q=barack%20obama&callback=JSON_CALLBACK')
-      .then(function(response) {
-        console.log(response);
-        self.search = response.data;
-      });
-    };
-  });
-*/
-
-/*
-  angular.module('daily-bias')
-
-  .controller('SearchController', function submitSearch($http) {
-      $http.get('https://ajax.googleapis.com/ajax/services/search/news?' + 'callback=JSON_CALLBACK', {
-          params: {
-            "version": "1.0",
-            "language": "en"
-          }
-          .then
-                console.log();
-              });
-
-        };
+  this.submitSearch = function(){
+      $http.jsonp('https://ajax.googleapis.com/ajax/services/search/news?v=1.0&q=barack%20obama&callback=JSON_CALLBACK')
+        .then(function(response) {
+          console.log(response);
+          self.search = response.data;
+        });
       };
-*/
+    });
+  */
 
+  /*
+    angular.module('daily-bias')
 
-//})(); //END IIFE
+    .controller('SearchController', function submitSearch($http) {
+        $http.get('https://ajax.googleapis.com/ajax/services/search/news?' + 'callback=JSON_CALLBACK', {
+            params: {
+              "version": "1.0",
+              "language": "en"
+            }
+            .then
+                  console.log();
+                });
+
+          };
+        };
+  */
